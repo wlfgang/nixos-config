c2c495306010860fc44456937671eb4db9125deb
wlfgang <14792753+wlfgang@users.noreply.github.com>
2024-05-18 07:07:48 -0400

    Enable flakes

--- configuration.nix
+++ configuration.nix
@@ -141,4 +141,6 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  # Enable flakes
+  nix.settings.experimental-features = ["nix-command" "flakes" ];
 }
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
